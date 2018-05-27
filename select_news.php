4925312bb047d1e897333f2c45de22a5c5ca5f10
Vlad <vlad.zah@ukr.net>
2018-05-27 09:06:34 +0300

    Parser 1.3.1. Test View Query

--- select_news.php
+++ select_news.php
@@ -3,12 +3,52 @@
 require_once ("lib/sql.php");
 
 
-
-$statement = $connection->prepare("SELECT table_content.description, description_anchors.text FROM table_content LEFT JOIN description_anchors ON table_content.id = description_anchors.id_content ORDER BY (SELECT )");
+$statement = $connection->prepare("SELECT table_content.description, description_anchors.id_content, description_anchors.text, description_anchors.src FROM table_content LEFT JOIN description_anchors ON table_content.id = description_anchors.id_content ORDER BY DATE(date)");
 $statement->execute();
-$content = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-var_dump($content);
+$replace_content = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+foreach ($replace_content as $k => $val){
+
+    while($position = strrpos($val["description"], $val["text"]))
+    {
+        $d = substr_replace($val["description"],"<a href=".$val["src"].">".$val["text"]."</a>",$position,strlen($val["text"]));
+        echo $d;
+        var_dump($d);
+        echo "-------------------------------------";
+        $val["description"] = substr($val["description"], 0, $position);
+        $updateDescription = $connection->prepare("UPDATE table_content SET description=? WHERE id=?");
+        $updateDescription->bindParam($d,$val["id_content"]);
+
+
+        /*array_push($seeks, $seek);
+        $haystack = substr($haystack, 0, $seek);*/
+    }
+}
+
+
+
+$s = $connection->prepare("SELECT * FROM table_content Order by DATE(date) limit 3");
+$s->execute();
+
+while ($content = $s->fetch(PDO::FETCH_OBJ)){
+    try{
+        echo $content->id;
+        echo "<h1>$content->title</h1>";
+        echo "<p>$content->date</p>";
+        echo "<div class='img'><img src='$content->img'></div>";
+        echo "<div>$content->description</div>";
+        echo "<hr>";
+    }
+    catch (Exception $e){
+        $e->getMessage();
+    }
+
+}
+
+
+//var_dump($content);
 
 /*foreach ($content as $el => $c){
     var_dump($c);
